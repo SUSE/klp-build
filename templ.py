815ee6da5d2cb52f6ec6f548d8b6b2b71cc173b1
Marcos Paulo de Souza <mpdesouza@suse.com>
2022-01-21 11:48:29 -0300

    Fixes for generating a livepatch template file for vmlinux
    The long term idea is to fill a file in the setup-bsc step to map the
    module, the CONFIG_ guards and other necessary info, instead of
    replicating it in each step, like we didn't know/need these informations
    in previous steps.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- templ.py
+++ templ.py
@@ -1,5 +1,6 @@
-import jinja2
 from datetime import datetime
+import jinja2
+import pathlib
 
 class Template:
     _mod = ''
@@ -9,13 +10,19 @@ class Template:
     _ktype = ''
     _env = None
 
-    def __init__(self, mod, bsc, cve, conf, ktype):
+    def __init__(self, mod, vmlinux, bsc, cve, conf, ktype):
         # TODO: get these values from a file created by prepare command
         self._mod = mod
         self._bsc = bsc
         self._cve = cve
         self._conf = conf
         self._ktype = ktype
+
+        if not mod and not vmlinux:
+            raise ValueError('Either --mod or --vmlinux needs to be specified')
+
+        if mod and vmlinux:
+            raise ValueError('You can\'t specify both --mod and --vmlinux')
 
         self._env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), \
                     trim_blocks=True)
@@ -24,14 +31,20 @@ class Template:
         fname = 'kgraft_patch' if self._ktype == 'kgr' else 'livepatch'
         fname = fname + '_' + self._bsc
 
+        bsc = pathlib.Path(self._bsc)
+        bsc.mkdir(exist_ok=True)
+
         for ext in ['h', 'c']:
             templ = self._env.get_template('lp-' + ext + '.j2')
 
             templ.globals['year'] = datetime.today().year
 
-            with open(fname + '.' + ext, 'w') as f:
-                f.write(templ.render(mod = self._mod,
-                                    bsc = self._bsc,
+            if self._mod:
+                templ.globals['mod'] = self._mod
+
+            lp_file = pathlib.Path(bsc, fname + '.' + ext)
+            with open(lp_file, 'w') as f:
+                f.write(templ.render(bsc = self._bsc,
                                     cve = self._cve,
                                     config = self._conf,
                                     ktype = self._ktype))
