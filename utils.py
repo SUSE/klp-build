eae112e3de812b7ca332d61370da99a205854d33
Marcos Paulo de Souza <mpdesouza@suse.com>
2022-03-14 16:28:44 -0300

    utils.py: Fix bsc argument, which is an int now
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- utils.py
+++ utils.py
@@ -31,8 +31,8 @@ class Setup:
 
         self._work = work_dir
 
-        self._bsc_num = re.search('([0-9]+)', bsc).group(1)
-        self._bsc = 'bsc' + self._bsc_num
+        self._bsc_num = str(bsc)
+        self._bsc = 'bsc' + str(bsc)
         self._bsc_path = pathlib.Path(self._work, self._bsc)
         if self._bsc_path.exists() and not self._bsc_path.is_dir():
             raise ValueError('--bsc needs to be a directory, or not to exist')
