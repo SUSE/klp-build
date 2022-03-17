895b5cd13651684997746eb7816b540b21556625
Marcos Paulo de Souza <mpdesouza@suse.com>
2022-03-17 14:58:44 -0300

    create-lp: Creating complete livepatches
    Up until now, the template manager would create a boilerplate code, and
    one by one, the sources generated by ccp were patched and copied to the final
    livepatch file, generated by kgr-templ.
    
    With this change, create-lp will use the template engine to create the
    one livepatch file per kernel source file, including the klp_funcs and
    other used structures.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- templ.py
+++ templ.py
@@ -9,13 +9,16 @@ import requests
 import textwrap
 
 class Template:
-    def __init__(self, bsc, work_dir, ktype):
+    def __init__(self, bsc, work_dir, cs):
         self._bsc = 'bsc' + str(bsc)
         conf = pathlib.Path(work_dir, self._bsc, 'conf.json')
         if not conf.is_file():
             raise ValueError('config.json not found in {}'.format(str(conf)))
 
-        self._ktype = ktype
+        codestreams = pathlib.Path(work_dir, self._bsc, 'codestreams.json')
+        if not codestreams.is_file():
+            raise ValueError('codestreams.json not found in {}'.format(str(codestreams)))
+
         with open(conf, 'r') as f:
             data = json.load(f)
             self._bsc_num = data['bsc']
@@ -23,6 +26,14 @@ class Template:
             self._cve = data['cve']
             self._conf = data['conf']
             self._commits = data['commits']
+
+        with open(codestreams, 'r') as f:
+            data = json.load(f)
+            jcs = data[cs]
+            self._ktype = jcs['rename_prefix']
+            self._files = list(jcs['files'].keys())
+            self._work_dirs = jcs['work_dir']
+
         try:
             conf = git.GitConfigParser()
             self._user = conf.get_value('user', 'name')
@@ -30,37 +41,63 @@ class Template:
         except:
             raise RuntimeError('Please define name/email in global git config')
 
-        fsloader = jinja2.FileSystemLoader(pathlib.Path(os.path.dirname(__file__), 'templates'))
-        self._env = jinja2.Environment(loader=fsloader, trim_blocks=True)
+        self._templ_path = pathlib.Path(os.path.dirname(__file__), 'templates')
+ 
+    def GenerateLivepatchFile(self, ext, fname, file_path, src_file, ext_file):
+        fsloader = jinja2.FileSystemLoader([self._templ_path, file_path])
+        env = jinja2.Environment(loader=fsloader, trim_blocks=True)
+
+        templ = env.get_template('lp-' + ext + '.j2')
+        templ.globals['year'] = datetime.today().year
+        if self._mod:
+            templ.globals['mod'] = self._mod
+
+        with open(pathlib.Path(self._bsc, fname), 'w') as f:
+            f.write(templ.render(bsc = self._bsc,
+                                bsc_num = self._bsc_num,
+                                inc_src_file = src_file,
+                                inc_exts_file = ext_file,
+                                cve = self._cve,
+                                config = self._conf,
+                                ktype = self._ktype,
+                                user = self._user,
+                                email = self._email,
+                                commits = self._commits))
 
     def GenerateLivePatches(self):
-        fname = 'kgr_patch' if self._ktype == 'kgr' else 'livepatch'
-        fname = fname + '_' + self._bsc
-
+        # If the livepatch contains only one file, generate only the livepatch
+        # one
         bsc = pathlib.Path(self._bsc)
         bsc.mkdir(exist_ok=True)
 
-        for ext in ['h', 'c']:
-            templ = self._env.get_template('lp-' + ext + '.j2')
+        # We need at least one header file for the livepatch
+        fname = 'kgr_patch' if self._ktype == 'kgr' else 'livepatch'
+        fname = fname + '_' + self._bsc
 
-            templ.globals['year'] = datetime.today().year
+        self.GenerateLivepatchFile('h', fname + '.h', '', None, None)
 
-            if self._mod:
-                templ.globals['mod'] = self._mod
+        # Run the template engine for each touched source file.
+        for index, wdir in enumerate(self._work_dirs):
+            src_name = self._bsc + '_' + pathlib.Path(self._files[index]).name
 
-            lp_file = pathlib.Path(bsc, fname + '.' + ext)
-            with open(lp_file, 'w') as f:
-                f.write(templ.render(bsc = self._bsc,
-                                    bsc_num = self._bsc_num,
-                                    cve = self._cve,
-                                    config = self._conf,
-                                    ktype = self._ktype,
-                                    user = self._user,
-                                    email = self._email,
-                                    commits = self._commits))
+            # If the livepatch touches only one source file, generate only a
+            # 'livepatch_' file, including the touched source file
+            _fname = src_name
+            if len(self._work_dirs) == 1:
+                _fname = fname + '.c'
+
+            self.GenerateLivepatchFile('c', _fname, wdir, src_name, 'exts')
+
+        # One additional file to encapsulate the _init and _clenaup methods
+        # of the other source files
+        if len(self._work_dirs) > 1:
+            self.GenerateLivepatchFile('c', fname + '.c', '', None, None)
 
     # Return the commit message in a list of wrapped
     def generate_commit_msg(self):
+        fsloader = jinja2.FileSystemLoader(self._templ_path)
+        self._env = jinja2.Environment(loader=fsloader, trim_blocks=True)
+
         templ = self._env.get_template('commit.j2')
         return templ.render(bsc = self._bsc,
                             bsc_num = self._bsc_num,
