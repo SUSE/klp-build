19848c688d6b0611b1d628fee3411e258adef1f2
Marcos Paulo de Souza <mpdesouza@suse.com>
2022-04-04 15:36:44 -0300

    templ.py: Add a condition to include suse commits header
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- templ.py
+++ templ.py
@@ -53,7 +53,8 @@ class Template:
                 f.write('{} {} klpp_{} IS_ENABLED({})\n'.format(mod, fun, fun,
                     self._conf))
 
-    def GenerateLivepatchFile(self, ext, out_name, src_file, ext_file):
+    def GenerateLivepatchFile(self, ext, out_name, src_file, ext_file,
+            include_header):
         if not out_name and not src_file:
             raise RuntimeError('Both out_name and src_file are empty.  Aborting.')
 
@@ -84,6 +85,9 @@ class Template:
         if self._mod:
             templ.globals['mod'] = self._mod
 
+        if include_header:
+            templ.globals['include_header'] = True
+
         with open(pathlib.Path(self.cfg.bsc, out_name).with_suffix('.' + ext), 'w') as f:
             f.write(templ.render(bsc = self.cfg.bsc,
                                 bsc_num = self.cfg.bsc_num,
@@ -107,19 +111,20 @@ class Template:
         out_name = 'kgr_patch' if self._ktype == 'kgr' else 'livepatch'
         out_name = out_name + '_' + self.cfg.bsc
 
-        self.GenerateLivepatchFile('h', out_name, None, None)
+        self.GenerateLivepatchFile('h', out_name, None, None, False)
 
         if len(self._files) == 1:
-            self.GenerateLivepatchFile('c', out_name, self._files[0], 'exts')
+            self.GenerateLivepatchFile('c', out_name, self._files[0], 'exts',
+                    True)
             return
 
         # Run the template engine for each touched source file.
         for src_file in self._files:
-            self.GenerateLivepatchFile('c', None, src_file, 'exts')
+            self.GenerateLivepatchFile('c', None, src_file, 'exts', False)
 
         # One additional file to encapsulate the _init and _clenaup methods
         # of the other source files
-        self.GenerateLivepatchFile('c', out_name, None, None)
+        self.GenerateLivepatchFile('c', out_name, None, None, True)
 
     # Return the commit message in a list of wrapped
     def generate_commit_msg(self):
