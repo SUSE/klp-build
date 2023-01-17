1cc7bfe3fb0ac90bcce344730c712fb7243eb97e
Marcos Paulo de Souza <mpdesouza@suse.com>
2023-01-17 10:52:59 -0300

    Add scripts/run-kgr-test.sh script
    Until now we would need to have the script on each testing server, and
    they have minor changes between them. Now on we will keep track of the
    changes of this file under the klp-build project.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

---
+++ scripts/run-kgr-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script is bundled together in the directory of tests, so the parent
+# directory of the current script is the bsc number
+bsc=$(basename "$(pwd)")
+
+# Remove any previous test output
+rm -rf tests.out/*
+
+# If the test has multiple files, it should be contained in a directory under
+# repro/$bsc, otherwise it's only one file under repro
+CONF_IN=repro/$bsc/config.in
+if [ ! -f "$CONF_IN" ]; then
+	CONF_IN=repro/${bsc}_config.in
+fi
+
+# On kunlun we are using Nicolai's vm files
+VMSDIR="$HOME/vms"
+if [ ! -d "$VMSDIR" ]; then
+	VMSDIR="/dev/shm/nstange/vms"
+fi
+
+# grab all vm.xml files regarding each main codestream
+xmls=""
+codestreams=$(cut -d _ -f 1 < "$CONF_IN" | uniq | tr '[:upper:]' '[:lower:]')
+for cs in $codestreams; do
+	xmls="$xmls -q $VMSDIR/$cs.xml"
+done
+
+JOBS=4
+
+~/kgr-test/kgr-test/kgr-test.py -s ~/scratch/ \
+					-r built \
+					-o tests.out \
+					-t repro \
+					$xmls \
+					-j $JOBS 2>&1 | tee testall.out
