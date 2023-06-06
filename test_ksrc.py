83feb1d84ce73826f7ba28b477f520f5c9536404
Marcos Paulo de Souza <mpdesouza@suse.com>
2023-06-06 11:44:30 -0300

    test_ksrc.py: Add first unittest
    The test checks for upstream commits messages with multiple lines
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

---
+++ test_ksrc.py
@@ -0,0 +1,11 @@
+import unittest
+from ksrc import GitHelper
+
+class GitHelperTesting(unittest.TestCase):
+    def test_multiline_upstream_commit_subject(self):
+        subj = GitHelper.get_commit_subject('49c47cc21b5b')
+        self.assertEqual('net: tls: fix possible race condition between '
+        'do_tls_getsockopt_conf() and do_tls_setsockopt_conf()', subj)
+
+if __name__ == '__main__':
+    unittest.main()
