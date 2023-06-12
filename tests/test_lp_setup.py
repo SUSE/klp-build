031b424d52a7161ef39a3a2c2dda3eb1dd9e7f9e
Marcos Paulo de Souza <mpdesouza@suse.com>
2023-06-12 14:02:11 -0300

    tests: Create TestUtils class that inherit from unittest
    Making possible to move some Setup functions to be later used by ccp
    module.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- tests/test_lp_setup.py
+++ tests/test_lp_setup.py
@@ -2,42 +2,22 @@ import json
 import logging
 from pathlib import Path
 import os
-import shutil
 import sys
 import unittest
-from unittest.mock import patch
 
 from tests import utils
 
 sys.path.append('..')
-from lp_setup import Setup
 
-class LpSetupTest(unittest.TestCase):
+class LpSetupTest(utils.TestUtils):
     def setUp(self):
         # Avoid searching for patches kernels
         os.environ['KLP_KERNEL_SOURCE'] = ''
 
         logging.disable(logging.INFO)
 
-    def setup(self, dargs, init = False):
-        with self.assertNoLogs(level='WARNING') as anl:
-            return utils.setup(dargs, init)
-
-    def setup_assert_logs(self, dargs, alevel, msg):
-        with self.assertLogs(level=alevel) as logs:
-            utils.setup(dargs, True)
-
-        self.assertRegex(logs.output[0], msg)
-
-    def setup_and_assert(self, dargs, exc, msg = None):
-        with self.assertRaises(exc) as ar:
-            utils.setup(dargs, True)
-
-        if not msg:
-            self.assertEqual(str(ar.exception), msg)
-
     def test_missing_conf_archs(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['archs'] = []
         self.setup_and_assert(v, ValueError,
                 'Please specify --conf when not all architectures are supported')
@@ -54,7 +34,7 @@ class LpSetupTest(unittest.TestCase):
                 'You need to specify at least one of the file-funcs variants!')
 
     def test_missing_conf_mod(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         self.setup_and_assert(v, ValueError,
                 'Please specify --conf when a module is specified')
@@ -65,32 +45,32 @@ class LpSetupTest(unittest.TestCase):
                 'You need to specify at least one of the file-funcs variants!')
 
     def test_missing_file_funcs(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         v['conf'] = 'CONFIG_TUN'
         self.setup_and_assert(v, ValueError,
                 'You need to specify at least one of the file-funcs variants!')
 
     def test_file_funcs_ok(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         v['conf'] = 'CONFIG_TUN'
         v['file_funcs'] = ['drivers/net/tun.c', 'tun_chr_ioctl', 'tun_free_netdev']
-        self.setup(v)
+        self.setup_nologs(v)
 
         # Checks if the variants of file-funcs also work
         v['file_funcs'] = []
         v['mod_file_funcs'] = [['tun', 'drivers/net/tun.c', 'tun_chr_ioctl',
                                 'tun_free_netdev']]
-        self.setup(v)
+        self.setup_nologs(v)
 
         v['mod_file_funcs'] = []
         v['conf_mod_file_funcs'] = [['CONFIG_TUN', 'tun', 'drivers/net/tun.c',
                                      'tun_chr_ioctl', 'tun_free_netdev']]
-        self.setup(v)
+        self.setup_nologs(v)
 
     def test_non_existent_file(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         v['conf'] = 'CONFIG_TUN'
         v['file_funcs'] = [['drivers/net/tuna.c', 'tun_chr_ioctl',
@@ -99,15 +79,15 @@ class LpSetupTest(unittest.TestCase):
         self.setup_and_assert(v, RuntimeError, 'File drivers/net/tuna.c not found')
 
     def test_existent_file(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         v['conf'] = 'CONFIG_TUN'
         v['file_funcs'] = [['drivers/net/tun.c', 'tun_chr_ioctl',
                             'tun_free_netdev']]
-        self.setup(v, True)
+        self.setup_nologs(v, True)
 
     def test_invalid_sym(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tun'
         v['conf'] = 'CONFIG_TUN'
         v['file_funcs'] = [['drivers/net/tun.c', 'tun_chr_ioctll',
@@ -117,7 +97,7 @@ class LpSetupTest(unittest.TestCase):
                 'Symbols tun_chr_ioctll not found on tun')
 
     def test_non_existent_module(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['module'] = 'tuna'
         v['conf'] = 'CONFIG_TUN'
         v['file_funcs'] = [['drivers/net/tun.c', 'tun_chr_ioctl',
@@ -126,12 +106,12 @@ class LpSetupTest(unittest.TestCase):
         self.setup_and_assert(v, RuntimeError, 'Module not found: tuna')
 
     def test_check_symbol_addr_s390(self):
-        v = utils.sargs()
+        v = self.sargs()
         v['filter'] = '12.4u35'
         v['module'] = 'sch_qfq'
         v['conf'] = 'CONFIG_NET_SCH_QFQ'
         v['file_funcs'] = [['net/sched/sch_qfq.c', 'qfq_change_class']]
-        s = self.setup(v, True)
+        s = self.setup_nologs(v, True)
 
         # The address of qfq_policy on s390x ends with a character, a bug that
         # was fixed by checking for \w instead of \d.
@@ -140,7 +120,7 @@ class LpSetupTest(unittest.TestCase):
                                               ['qfq_policy']))
 
     def test_check_conf_mod_file_funcs(self):
-        v = utils.sargs()
+        v = self.sargs()
         cs = '15.4u12'
         v['archs'] = ['x86_64']
         v['filter'] = cs
@@ -152,9 +132,9 @@ class LpSetupTest(unittest.TestCase):
                                  'btsdio_probe', 'btsdio_remove' ]]
 
         # CONF should be same, but mod doesn't
-        self.setup(v, True)
+        self.setup_nologs(v, True)
 
-        with open(Path(utils.basedir(v), 'codestreams.json')) as f:
+        with open(Path(self.basedir(v), 'codestreams.json')) as f:
             data = json.loads(f.read())[cs]['files']
 
         sch = data['net/sched/sch_qfq.c']
@@ -169,9 +149,9 @@ class LpSetupTest(unittest.TestCase):
                                      'btsdio_probe', 'btsdio_remove' ]]
 
         # Now, conf and module should be different
-        s = self.setup(v, True)
+        s = self.setup_nologs(v, True)
 
-        with open(Path(utils.basedir(v), 'codestreams.json')) as f:
+        with open(Path(self.basedir(v), 'codestreams.json')) as f:
             data = json.loads(f.read())[cs]['files']
 
         sch = data['net/sched/sch_qfq.c']
--- tests/utils.py
+++ tests/utils.py
@@ -2,35 +2,53 @@ import os
 from pathlib import Path
 import shutil
 import sys
+import unittest
 
 sys.path.append('..')
 from lp_setup import Setup
 
-SETUP_ARGS = {
-    'bsc' : '9999999',
-    'filter' : '',
-    'cve' : '1234-5678',
-    'cs' : '',
-    'file_funcs' : [],
-    'mod_file_funcs' : [],
-    'conf_mod_file_funcs' : [],
-    'module' : 'vmlinux',
-    'conf' : '',
-    'archs': ['x86_64', 'ppc64le', 's390x']
-}
+class TestUtils(unittest.TestCase):
 
-# default setup args
-def sargs():
-    return SETUP_ARGS.copy()
+    # default setup args
+    def sargs(self):
+        return {
+            'bsc' : '9999999',
+            'filter' : '',
+            'cve' : '1234-5678',
+            'cs' : '',
+            'file_funcs' : [],
+            'mod_file_funcs' : [],
+            'conf_mod_file_funcs' : [],
+            'module' : 'vmlinux',
+            'conf' : '',
+            'archs': ['x86_64', 'ppc64le', 's390x']
+        }
 
-def basedir(v):
-    return Path(os.getenv('KLP_WORK_DIR', ''), f'bsc{v["bsc"]}')
+    def basedir(self, v):
+        return Path(os.getenv('KLP_WORK_DIR', ''), f'bsc{v["bsc"]}')
 
-def setup(dargs, init = False):
-    shutil.rmtree(basedir(dargs), ignore_errors=True)
+    def setup(self, dargs, init = False):
+        shutil.rmtree(self.basedir(dargs), ignore_errors=True)
 
-    s = Setup(*tuple(dargs.values()))
-    if init:
-        s.setup_project_files()
+        s = Setup(*tuple(dargs.values()))
+        if init:
+            s.setup_project_files()
 
-    return s
+        return s
+
+    def setup_nologs(self, dargs, init = False):
+        with self.assertNoLogs(level='WARNING') as anl:
+            return self.setup(dargs, init)
+
+    def setup_assert_logs(self, dargs, alevel, msg):
+        with self.assertLogs(level=alevel) as logs:
+            self.setup(dargs, True)
+
+        self.assertRegex(logs.output[0], msg)
+
+    def setup_and_assert(self, dargs, exc, msg = None):
+        with self.assertRaises(exc) as ar:
+            self.setup(dargs, True)
+
+        if not msg:
+            self.assertEqual(str(ar.exception), msg)
