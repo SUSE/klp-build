dd9096aee0f924acfa64cb1462738cbab8945252
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-02-19 14:49:25 -0300

    config.py: Fix rmtree missing )
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- config.py
+++ config.py
@@ -115,7 +115,7 @@ class Config:
             raise RuntimeError(f'{cs}: quilt pop failed: {err.stderr}')
 
         shutil.rmtree(patches_dir)
-        shutil.rmtree(Path(sdir, ".pc")
+        shutil.rmtree(Path(sdir, ".pc"))
 
     def apply_all_patches(self, cs, fil=subprocess.STDOUT):
         patched = False
