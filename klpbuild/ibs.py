SUSE/klp-build

commit fb40c1c5a80282d2c7edb5058610d1f317c6f4e5

Change get_cs_boot_file to target odir instead of arch/boot dir
This way we can rely on files that are already extracted, like
Module.synmvers vs boot/symvers-{kver}.gz.

Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

klpbuild/ibs.py
@@ -212,7 +212,7 @@ class IBS(Config):
                 subprocess.check_output(rf'find {vmlinux_path} -name "*gz" -exec gzip -d -f {{}} \;', shell=True)
 
             # Use the SLE .config
-            shutil.copy(self.get_cs_boot_file(cs, "config"), Path(self.get_odir(cs), ".config"))
+            shutil.copy(self.get_cs_boot_file(cs, ".config"), Path(self.get_odir(cs), ".config"))
 
             # Recreate the build link to enable us to test the generated LP
             mod_path = Path(self.get_mod_path(cs, ARCH), "build")

klpbuild/ccp.py
@@ -182,7 +182,7 @@ class CCP(Config):
         # Needed, otherwise threads would interfere with each other
         env = self.env.copy()
 
-        env["KCP_MOD_SYMVERS"] = str(self.get_cs_boot_file(cs, "symvers"))
+        env["KCP_MOD_SYMVERS"] = str(self.get_cs_boot_file(cs, "Module.symvers"))
         env["KCP_KBUILD_ODIR"] = str(odir)
         env["KCP_PATCHED_OBJ"] = self.get_module_obj(ARCH, cs, fdata["module"])
         env["KCP_KBUILD_SDIR"] = str(sdir)

klpbuild/ce.py
@@ -63,7 +63,7 @@ class CE(Config):
         ce_args.extend(
             [
                 f'-DCE_DEBUGINFO_PATH={self.get_module_obj(ARCH, cs, fdata["module"])}',
-                f'-DCE_SYMVERS_PATH={self.get_cs_boot_file(cs, "symvers")}',
+                f'-DCE_SYMVERS_PATH={self.get_cs_boot_file(cs, "Module.symvers")}',
                 f"-DCE_OUTPUT_FILE={Path(out_dir, self.lp_out_file(fname))}",
                 f'-DCE_OUTPUT_FUNCTION_PROTOTYPE_HEADER={Path(out_dir, "proto.h")}',
                 f'-DCE_DSC_OUTPUT={Path(out_dir, "lp.dsc")}',

klpbuild/config.py
@@ -214,7 +214,7 @@ class Config:
             if arch not in self.get_cs_archs(cs):
                 continue
 
-            kconf = self.get_cs_boot_file(cs, "config", arch)
+            kconf = self.get_cs_boot_file(cs, ".config", arch)
             with open(kconf) as f:
                 match = re.search(rf"{conf}=([ym])", f.read())
                 if not match:
@@ -235,7 +235,7 @@ class Config:
             raise RuntimeError(f"Configuration mismtach between codestreams. Aborting.")
 
     def missing_codestream(self, cs):
-        return not self.get_cs_boot_file(cs, "config").exists()
+        return not self.get_cs_boot_file(cs, ".config").exists()
 
     def cs_is_rt(self, cs):
         return self.get_cs_data(cs).get("rt", False)
@@ -244,15 +244,12 @@ class Config:
         return "rt" if self.cs_is_rt(cs) else "default"
 
     def get_cs_boot_file(self, cs, file, arch=""):
-        if self.kdir:
-            if file == "symvers":
-                return Path(self.data, "Module.symvers")
-            return Path(self.data, file)
+        if file == "vmlinux":
+            if self.kdir:
+                return Path(self.get_data_dir(arch), file)
+            return Path(self.get_data_dir(arch), "boot", f"{file}-{self.get_cs_kernel(cs)}-{self.get_ktype(cs)}")
 
-        if not arch:
-            arch = ARCH
-
-        return Path(self.get_data_dir(arch), "boot", f"{file}-{self.get_cs_kernel(cs)}-{self.get_ktype(cs)}")
+        return Path(self.get_odir(cs, arch), file)
 
     def get_data_dir(self, arch):
         if self.kdir:
@@ -260,7 +257,7 @@ class Config:
 
         return Path(self.data, arch)
 
-    def get_sdir(self, cs):
+    def get_sdir(self, cs, arch=""):
         if self.kdir:
             return self.data
 
@@ -268,12 +265,16 @@ class Config:
         ktype = f"-{self.get_ktype(cs)}"
         if ktype == "-default":
             ktype = ""
-        return Path(self.get_data_dir(ARCH), "usr", "src", f"linux-{self.get_cs_kernel(cs)}{ktype}")
 
-    def get_odir(self, cs):
+        if not arch:
+            arch = ARCH
+
+        return Path(self.get_data_dir(arch), "usr", "src", f"linux-{self.get_cs_kernel(cs)}{ktype}")
+
+    def get_odir(self, cs, arch=""):
         if self.kdir:
             return self.data
-        return Path(f"{self.get_sdir(cs)}-obj", ARCH, self.get_ktype(cs))
+        return Path(f"{self.get_sdir(cs, arch)}-obj", ARCH, self.get_ktype(cs))
 
     def get_ipa_file(self, cs, fname):
         if self.kdir:

klpbuild/inline.py
@@ -41,7 +41,7 @@ class Inliner(Config):
         if ipa_f.exists():
             ce_args.extend(["-ipa-files", str(ipa_f)])
 
-        ce_args.extend(["-symvers", str(self.get_cs_boot_file(cs, "symvers"))])
+        ce_args.extend(["-symvers", str(self.get_cs_boot_file(cs, "Module.symvers"))])
 
         ce_args.extend([func])
 

commit 30e27dfd910ac515f7faf5876982991cf03bdeb8

ibs.py: Do not create a second time the ".config"
Once the kernel and related binaries get extracted so does the
".config", which gets copied twice to its respective kernel
src directory. Only once is good enough.

This commit also fixes a bug introduced in fb40c1c5, where the
".config" file was copied from a source with the same path
as the destination, hence leading to a crash.

Signed-off-by: Fernando Gonzalez <fernando.gonzalez@suse.com>

klpbuild/ibs.py
@@ -200,9 +200,6 @@ class IBS(Config):
                 vmlinux_path = Path(self.get_data_dir(arch), "boot")
                 subprocess.check_output(rf'find {vmlinux_path} -name "*gz" -exec gzip -d -f {{}} \;', shell=True)
 
-            # Use the SLE .config
-            shutil.copy(self.get_cs_boot_file(cs, ".config"), Path(self.get_odir(cs), ".config"))
-
             # Recreate the build link to enable us to test the generated LP
             mod_path = Path(self.get_mod_path(cs, ARCH), "build")
             mod_path.unlink()