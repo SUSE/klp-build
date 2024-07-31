f503937ac8a110c0ea1b7d5267a701aff5eba670
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-07-31 17:36:39 -0300

    test.sh: Simplify call to setup_extract_count
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- tests/test.sh
+++ tests/test.sh
@@ -14,9 +14,9 @@ KLP_GIT_SOURCE=/home/mpdesouza/git/linux
 
 setup_extract_count()
 {
-	local max_lines="$1"
+	local lp_name="$1"
 	shift
-	local patch_="$1"
+	local max_lines="$1"
 	shift
 	local conf_="$1"
 	shift
@@ -28,37 +28,40 @@ setup_extract_count()
 
 	echo
 	echo
-	echo "Extracting $patch_"
+	echo "Extracting $lp_name"
 
-	rm -rf $KLP_WORK_DIR/$patch_
+	rm -rf $KLP_WORK_DIR/$lp_name
 	if [ -n "$mod_" ]; then
-		klp-build setup --name $patch_ --kdir --data-dir $KLP_GIT_SOURCE --conf $conf_ --module $mod_ --file-funcs $file_ $@
+		klp-build setup --name $lp_name --kdir --data-dir $KLP_GIT_SOURCE --conf $conf_ --module $mod_ --file-funcs $file_ $@
 	else
-		klp-build setup --name $patch_ --kdir --data-dir $KLP_GIT_SOURCE --conf $conf_ --file-funcs $file_ $@
+		klp-build setup --name $lp_name --kdir --data-dir $KLP_GIT_SOURCE --conf $conf_ --file-funcs $file_ $@
 	fi
-	klp-build extract --name $patch_ --type ce
+	klp-build extract --name $lp_name --type ce
 	# Check if the extracted code compiles
-	make -C $KLP_WORK_DIR/$patch_/ce/linux/lp
+	make -C $KLP_WORK_DIR/$lp_name/ce/linux/lp
 
-	nlines=$(cat $KLP_WORK_DIR/$patch_/ce/linux/lp/livepatch_${patch_}.c | wc -l)
+	nlines=$(cat $KLP_WORK_DIR/$lp_name/ce/linux/lp/livepatch_${lp_name}.c | wc -l)
 	if [ "$nlines" -gt "$max_lines" ]; then
-		echo "ERROR: Generated file for $patch_ contains $nlines LoC, but should contain < $max_lines LoC"
+		echo "ERROR: Generated file for $lp_name contains $nlines LoC, but should contain < $max_lines LoC"
 		exit 1
 	fi
 }
 
-setup_extract_count "105" lp_proc_cmdline_show	CONFIG_PROC_FS	""  fs/proc/cmdline.c	cmdline_proc_show
+# FIXME: we should take a look into it to reduce the number of lines generated
+# for:
+#	lp_cve_2024_35950
+#	lp_cve_2021_47378
+LPS=$(cat << EOF
+lp_proc_cmdline_show		105 CONFIG_PROC_FS	vmlinux		fs/proc/cmdline.c cmdline_proc_show
+lp_cve_2021_22600		640 CONFIG_UNIX		af_packet	net/packet/af_packet.c	packet_set_ring
+lp_ipv6_route_multipath_add	700 CONFIG_IPV6		ipv6		net/ipv6/route.c	ip6_route_multipath_add
+lp_cve_2024_27398		88  CONFIG_BT		bluetooth	net/bluetooth/sco.c sco_sock_timeout
+lp_cve_2024_26923		170 CONFIG_UNIX		vmlinux		net/unix/garbage.c	unix_gc
+lp_cve_2024_35950		830 CONFIG_DRM		vmlinux		drivers/gpu/drm/drm_client_modeset.c	drm_client_modeset_probe
+lp_cve_2021_47378		2832 CONFIG_NVME_RDMA	nvme-rdma	drivers/nvme/host/rdma.c	nvme_rdma_free_queue nvme_rdma_cm_handler
+EOF
+)
 
-setup_extract_count "640" lp_cve_2021_22600	CONFIG_UNIX	af_packet  net/packet/af_packet.c	packet_set_ring
-
-setup_extract_count "700" lp_ipv6_route_multipath_add	CONFIG_IPV6	ipv6	net/ipv6/route.c	ip6_route_multipath_add
-
-setup_extract_count "88" lp_cve_2024_27398	CONFIG_BT	bluetooth  net/bluetooth/sco.c sco_sock_timeout
-
-setup_extract_count "170" lp_cve_2024_26923	CONFIG_UNIX	""	net/unix/garbage.c	unix_gc
-
-# FIXME: we should take a look into it to reduce the number of lines generated.
-setup_extract_count "830" lp_cve_2024_35950	CONFIG_DRM	""	drivers/gpu/drm/drm_client_modeset.c	drm_client_modeset_probe
-
-# FIXME: we should take a look into it to reduce the number of lines generated.
-setup_extract_count "2832" lp_cve_2021_47378	CONFIG_NVME_RDMA	nvme-rdma	drivers/nvme/host/rdma.c	nvme_rdma_free_queue nvme_rdma_cm_handler
+while IFS=, read lp_name loc config mod file funcs; do
+	setup_extract_count $lp_name $loc $config $mod $file $funcs
+done <<< "$LPS"
