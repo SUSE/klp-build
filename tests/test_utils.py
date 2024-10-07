fd2c6bf6e0ee96a5201b3d927997def8a4ad81c3
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-10-07 16:38:35 -0300

    test: utils: Add check for the new Codestream classify
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,12 @@
 import klpbuild.utils as utils
+from klpbuild.codestream import Codestream
 
 def test_group_classify():
     assert utils.classify_codestreams(["15.2u10", "15.2u11", "15.3u10", "15.3u12"]) == \
+                                        ["15.2u10-11", "15.3u10-12"]
+
+    assert utils.classify_codestreams([Codestream(15, 2, 10, False),
+                                        Codestream(15, 2, 11, False),
+                                        Codestream(15, 3, 10, False),
+                                        Codestream(15, 3, 12, False)]) == \
                                         ["15.2u10-11", "15.3u10-12"]
