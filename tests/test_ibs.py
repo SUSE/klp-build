570a4d72bf7488ab6aa3cd568d111471cb2ea11d
Marcos Paulo de Souza <mpdesouza@suse.com>
2025-01-28 13:30:30 -0300

    ibs: Test the procedure of getting kernel rpm packages
    The functionality was extracted from download_cs_data into a new function so it
    can be tested more easily.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

---
+++ tests/test_ibs.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2025 SUSE
+# Author: Marcos Paulo de Souza <mpdesouza@suse.com>
+
+import inspect
+from pathlib import Path
+
+from klpbuild.klplib.ibs import IBS
+from klpbuild.plugins.setup import Setup
+
+CS = "15.6u0"
+DEFAULT_DATA = {"cve": None, "lp_filter": CS, "lp_skips": None, "conf": None, "no_check": False}
+
+
+def test_list_of_packages():
+    # Check if the package gathering mechanism works
+    lp = "bsc_" + inspect.currentframe().f_code.co_name
+    lp_setup = Setup(lp)
+    codestreams = lp_setup.setup_codestreams(DEFAULT_DATA)
+    assert len(IBS(lp, CS).get_cs_packages(codestreams, Path("random"))) > 0
