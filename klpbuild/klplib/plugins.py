721728d0ec4cf235cd1173ec155ee43a70195a41
Vincenzo MEZZELA <vincenzo.mezzela@suse.com>
2025-02-04 10:43:35 +0100

    klplib: plugins: add helper to extract the required plugin arguments
    Signed-off-by: Vincenzo MEZZELA <vincenzo.mezzela@suse.com>

--- klpbuild/klplib/plugins.py
+++ klpbuild/klplib/plugins.py
@@ -3,7 +3,9 @@
 # Copyright (C) 2025 SUSE
 # Author: Vincenzo Mezzela <vincenzo.mezzela@suse.com>
 
+import argparse
 import importlib
+import inspect
 import logging
 
 PLUGINS_PATH = "klpbuild.plugins."
@@ -15,7 +17,7 @@ def try_run_plugin(name, args):
 
     Args:
         name (str): The name of the plugin module to import.
-        args (Any): The arguments to pass to the `run` function of the plugin.
+        args (Any): The main argument parser.
 
     Raises:
         AssertionError: If the module does not have a `run` function.
@@ -26,4 +28,27 @@ def try_run_plugin(name, args):
     module = importlib.import_module(PLUGINS_PATH + name)
     assert hasattr(module, "run"), f"Module {name} is not a plugin!"
 
-    module.run(args)
+    run_func = getattr(module, "run")
+    required_args = __get_required_plugin_args(run_func, args)
+    run_func(**required_args)
+
+
+def __get_required_plugin_args(func, args):
+    """
+    Extracts arguments from an `argparse.Namespace` object and returns only
+    those that are needed as keyword arguments by the function.
+
+    Args:
+        func (function): The name of the plugin module to import.
+        args (ArgumentParser): The main argument parser.
+
+    Raises:
+        AssertionError: If the provided argument is not an instance of
+        `argparse.Namespace`.
+
+    """
+    assert isinstance(args, argparse.Namespace)
+
+    all_args = vars(args)
+    required_args_names = inspect.getfullargspec(func).args
+    return  {arg_name: all_args.get(arg_name, None) for arg_name in required_args_names}
