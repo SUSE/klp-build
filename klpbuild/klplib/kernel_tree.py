94e35370b8330a7bdd554937d6302375e4c2ab9a
Vincenzo MEZZELA <vincenzo.mezzela@suse.com>
2025-03-20 13:12:26 +0100

    plugins: introduce clean-sources plugin
    Signed-off-by: Vincenzo MEZZELA <vincenzo.mezzela@suse.com>

--- klpbuild/klplib/kernel_tree.py
+++ klpbuild/klplib/kernel_tree.py
@@ -5,9 +5,12 @@
 
 import logging
 import os
+import shutil
 import subprocess
 
 from klpbuild.klplib.config import get_user_path
+from klpbuild.klplib.utils import ARCH
+
 
 def init_cs_kernel_tree(kernel_version, outdir):
     """
@@ -35,6 +38,30 @@ def init_cs_kernel_tree(kernel_version, outdir):
         ], stderr=subprocess.PIPE)
 
 
+def cleanup_kernel_trees():
+    """
+    Remove kernel source trees.
+    """
+    kernel_tree = get_user_path("kernel_dir")
+    worktrees = __get_active_worktrees(kernel_tree)
+
+    for wt in worktrees:
+        logging.info("Removing worktree %s", wt)
+        __remove_worktree(kernel_tree, wt)
+
+    # Sometimes when git-worktree is killed during the checkout of the worktree
+    # there are pending sources which are not registered as worktree
+    sources_dir = get_user_path("data_dir")/ARCH/"usr"/"src"
+    for entry in os.listdir(sources_dir):
+        full_path = sources_dir/entry
+        if os.path.isdir(full_path) and not entry.endswith("-obj"):
+            logging.info(f"Removing pending kernel tree: {full_path}")
+            shutil.rmtree(full_path)
+
+    __prune_worktrees(kernel_tree)
+    assert not __get_active_worktrees(kernel_tree)
+
+
 def file_exists_in_tag(kernel_version, file_path):
     """
     Check if a specific file exists in a given kernel version tag.
@@ -52,3 +79,34 @@ def file_exists_in_tag(kernel_version, file_path):
     subprocess.check_output([
         'git',  "-C", kernel_tree, 'ls-tree', kernel_tree_git_tag, file_path
     ], stderr=subprocess.PIPE)
+
+
+def __get_active_worktrees(kernel_tree):
+    data_dir = str(get_user_path("data_dir"))
+    worktrees_output = subprocess.run(["git", "-C", kernel_tree, "worktree", "list"], capture_output=True, text=True)
+
+    worktrees = []
+    for line in worktrees_output.stdout.strip().split("\n"):
+        if data_dir not in line:
+            continue
+        if "prunable" in line:
+            continue
+        worktrees.append(line.split()[0])
+
+    return worktrees
+
+
+def __remove_worktree(kernel_tree, worktree_dir):
+    subprocess.check_output([ "/usr/bin/git", "-C", kernel_tree, "worktree",
+                             "remove", worktree_dir, "-f", "-f"],
+                            stderr=subprocess.PIPE)
+
+    if os.path.isdir(worktree_dir):
+        shutil.rmtree(worktree_dir)
+
+
+def __prune_worktrees(kernel_tree):
+    logging.debug("Pruning pending kernel worktrees")
+    subprocess.check_output([ "/usr/bin/git", "-C", kernel_tree, "worktree",
+                             "prune"],
+                            stderr=subprocess.PIPE)
