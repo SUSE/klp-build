eca1f1cb31ae79959e9ad99ed814948ac4d0c791
Marcos Paulo de Souza <mpdesouza@suse.com>
2025-04-23 10:29:38 -0300

    ibs.py: Remove workers attribute
    It's used only in one place, so make it a local variable.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- klpbuild/klplib/ibs.py
+++ klpbuild/klplib/ibs.py
@@ -38,8 +38,6 @@ class IBS():
         self.ibs_user = self.osc.username
         self.prj_prefix = f"home:{self.ibs_user}:{self.lp_name}-klp"
 
-        self.workers = int(get_user_settings("workers"))
-
         # Total number of work items
         self.total = 0
 
@@ -50,7 +48,8 @@ class IBS():
         if len(args) == 0:
             return
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
+        workers = int(get_user_settings("workers"))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
             results = executor.map(func, args)
             for result in results:
                 if result:
