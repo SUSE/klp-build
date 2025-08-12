c3cfff5ef432f877d8157b1af3f028dd6951a5b9
Fernando Gonzalez <fernando.gonzalez@suse.com>
2025-08-12 08:50:02 +0100

    tests: Fix and add bugzilla tests
    Signed-off-by: Fernando Gonzalez <fernando.gonzalez@suse.com>

--- tests/test_bugzilla.py
+++ tests/test_bugzilla.py
@@ -3,12 +3,30 @@
 # Copyright (C) 2025 SUSE
 # Author: Marcos Paulo de Souza <mpdesouza@suse.com>
 
-from klpbuild.klplib.bugzilla import get_bug_title
-from klpbuild.klplib.utils import get_lp_number
+from klpbuild.klplib.bugzilla import (get_bug, get_bug_title, get_pending_bugs,
+                                      get_bug_data, get_bug_dep, get_bug_desc)
+
+def test_get_pending_bugs():
+    bugs = get_pending_bugs()
+    assert len(bugs) > 0
+
 
 def test_get_bug_title():
-    assert get_bug_title("1227320") == "wifi: mac80211: check/clear fast rx for non-4addr sta VLAN changes"
-    assert get_bug_title(get_lp_number("bsc1227320")
-                         ) == "wifi: mac80211: check/clear fast rx for non-4addr sta VLAN changes"
-    assert get_bug_title("bla") == "Change me!"
-    assert get_bug_title(get_lp_number("bla")) == "Change me!"
+    bug = get_bug("1227320")
+    assert bug and get_bug_title(bug) == "wifi: mac80211: check/clear fast rx for non-4addr sta VLAN changes"
+    assert get_bug_title(get_bug("blah")) == "Change me!"
+
+
+def test_get_bug_data():
+    bug = get_bug("1227320")
+    cve, subsys, cvss, level, prio = get_bug_data(bug)
+    assert (cve == "2024-35789" and subsys == "wifi"
+            and cvss == "7.8" and level == "trivial"
+            and prio == "Medium")
+
+
+def test_get_bug_desc():
+    expected = "In the Linux kernel, the following vulnerability has been resolved:"
+    bug = get_bug("1227320")
+    desc = get_bug_desc(bug)
+    assert desc and expected in desc
