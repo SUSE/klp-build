693dfc8a4e4e3433dcc42a0ef5a887a7e209edcd
Fernando Gonzalez <fernando.gonzalez@suse.com>
2025-10-05 19:29:16 +0100

    klplib/file2config: Check parent CONFIG if 'obj-y'
    "obj-y" objects might be builtin into parent modules
    with a specific CONFIG.
    
    Also, skip header files.
    
    Signed-off-by: Fernando Gonzalez <fernando.gonzalez@suse.com>

--- klpbuild/klplib/file2config.py
+++ klpbuild/klplib/file2config.py
@@ -33,11 +33,14 @@ def _find_config(cs, base_dir, relative_obj_path, deep):
     if deep > 10:
         return None, ""
 
+    if Path(".") == base_dir:
+        return "CONFIG_SUSE_KERNEL", "vmlinux"
+
     make_file = Path(base_dir, "Makefile")
 
     lines = _load_makefile(cs, make_file)
 
-    if not lines and Path(".") != base_dir:
+    if not lines:
         relative_obj_path = base_dir.name + "/" + relative_obj_path
         return _find_config(cs, base_dir.parent, relative_obj_path, deep+1)
 
@@ -48,6 +51,9 @@ def _find_config(cs, base_dir, relative_obj_path, deep):
 
         # target found, check if this one with config
         target = sep[0]
+        if target.startswith('obj-y'):
+            # If it's built-in then check the config of the parent directory
+            return _find_config(cs, base_dir.parent, base_dir.name + '/', deep)
         if target.startswith('obj-'):
             return _sanitize_config(target), str((base_dir/relative_obj_path).with_suffix(''))
 
@@ -70,18 +76,19 @@ def find_configs_for_files(cs, file_paths: list):
     missing = []
 
     if not file_paths:
-        return configs, build_ins, missing
+        return configs, missing
 
     for path in file_paths:
         path = path.strip()
+        # Do not check headers
+        if path.endswith('h'):
+            continue
         obj_file = Path(path.replace('.c', '.o'))
         config, obj = _find_config(cs, obj_file.parent, obj_file.name, 0)
         if not config:
             missing.append(path)
-        elif config == 'y' or config == 'm':
-            build_ins.append(path)
         elif config.startswith('CONFIG_'):
             configs[path] = {'config': config, 'obj': obj}
         # else there is garbage like 'subst', 'vds' for wrongly parsed input
 
-    return configs, build_ins, missing
+    return configs, missing
