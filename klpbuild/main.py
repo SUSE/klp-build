dbcf7d94d7409bc472dd2c8a4c60bbc0e530fcd8
Vincenzo Mezzela <vincenzo.mezzela@suse.com>
2025-10-09 14:37:15 +0200

    klplib: implement persistent function cache
    Signed-off-by: Vincenzo Mezzela <vincenzo.mezzela@suse.com>

--- klpbuild/main.py
+++ klpbuild/main.py
@@ -6,8 +6,10 @@
 import logging
 import sys
 
+from klpbuild.klplib.cache import init_cache
 from klpbuild.klplib.cmd import create_parser
 from klpbuild.klplib.codestreams_data import load_codestreams
+from klpbuild.klplib.config import get_user_settings
 from klpbuild.klplib.plugins import try_run_plugin
 
 
@@ -17,6 +19,10 @@ def main():
 
     logging_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=logging_level, format="%(message)s")
+
+    # TODO: implement get_user_settings_bool()
+    cache_enabled = get_user_settings("cache", isopt=True) == "True" and (not args.nocache)
+    init_cache(cache_enabled)
 
     if hasattr(args, 'lp_name'):
         load_codestreams(args.lp_name)
