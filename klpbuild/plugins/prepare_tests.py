9d2d7c3f05067305c02db9212184c1e3b9084882
Marcos Paulo de Souza <mpdesouza@suse.com>
2026-04-09 17:10:36 -0300

    plugins: prepare_tests: Check if the test_src was found
    Otherwise we might fail is the script isn't found, which is not desired.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- klpbuild/plugins/prepare_tests.py
+++ klpbuild/plugins/prepare_tests.py
@@ -77,7 +77,7 @@ def download_built_rpms(lp_name, lp_filter):
 
 def prepare_tests(lp_name, lp_filter):
     test_src = get_tests_path(lp_name)
-    if not os.access(test_src, os.X_OK):
+    if test_src and not os.access(test_src, os.X_OK):
         logging.error("Script %s has no execution bit set. Aborting", test_src)
         sys.exit(1)
 
