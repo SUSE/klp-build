3ef16d5e2e532ace734161ccfb6f99b3baf341f1
Vincenzo Mezzela <vincenzo.mezzela@suse.com>
2026-04-13 08:52:14 +0000

    setup.py: support KLP_BUILD_VERSION env var for tarball builds

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import os
+
 import setuptools
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+# When building from a tarball (e.g. RPM packaging), set KLP_BUILD_VERSION
+# to inject the version statically. When unset, setuptools-git-versioning
+# derives it from git tags automatically.
+_version = os.environ.get("KLP_BUILD_VERSION")
+_git_versioning = {}
+_setup_requires = []
+
+if not _version:
+    _git_versioning = {"enabled": True}
+    _setup_requires = ["setuptools-git-versioning>=2.0,<3"]
 
 setuptools.setup(
     name="klp-build",
+    version=_version,
     author="Marcos Paulo de Souza",
     author_email="mpdesouza@suse.com",
     description="The kernel livepatching creation tool",
@@ -40,10 +53,6 @@ setuptools.setup(
         "tabulate",
         "termcolor"
     ],
-    setuptools_git_versioning={
-        "enabled": True,
-    },
-    setup_requires=[
-        "setuptools-git-versioning>=2.0,<3"
-    ],
+    setuptools_git_versioning=_git_versioning,
+    setup_requires=_setup_requires,
 )
