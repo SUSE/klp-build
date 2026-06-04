0803c1f29d2a452165978dd419580c289a787c00
Fernando Gonzalez <fernando.gonzalez@suse.com>
2026-06-04 13:46:43 +0100

    klplib/utils: Protect data_dir access
    Previous locking mechanism had become useless after
    the git-fs integrations as it only protected from concurrent
    extracts. Now most operations are read-only (extract, setup etc.)
    and can be run simultaneously without any danger.
    
    The new design allows having simultaneous readers or a single
    writer when required. Quite useful if multiple users want
    to share a single `data_dir` due to disk space constraints.
    
    Signed-off-by: Fernando Gonzalez <fernando.gonzalez@suse.com>

--- klpbuild/plugins/extract.py
+++ klpbuild/plugins/extract.py
@@ -16,12 +16,11 @@ from collections import OrderedDict
 from pathlib import Path
 from pathlib import PurePath
 from threading import Lock
-from filelock import FileLock
-
 from natsort import natsorted
 
 from klpbuild.klplib import utils
 from klpbuild.klplib.affected_file import AffectedFile, AffectedModule
+from klpbuild.klplib.utils import data_lock
 from klpbuild.klplib.cmd import add_arg_lp_name, add_arg_lp_filter
 from klpbuild.klplib.codestreams_data import store_codestreams, get_codestreams_data, get_codestreams_list
 from klpbuild.klplib.config import get_user_settings
@@ -779,9 +778,7 @@ def lp_out_cleanup(cs, fdata: AffectedFile, lp_out, sdir):
 
 
 def extract(lp_name, lp_filter, no_patches, avoid_ext):
-    sdir_lock = FileLock(utils.get_datadir()/"data.lock")
-
-    with sdir_lock:
+    with data_lock():
         start_extract(lp_name, lp_filter, no_patches, avoid_ext)
 
 
