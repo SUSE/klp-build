7dd54d5883e142462010fced68079c5b72fc2469
Marcos Paulo de Souza <mpdesouza@suse.com>
2026-06-15 11:15:25 -0300

    prepare_tests: Parallelize the module check
    When running prepare_tests for livepatches that affect all codestreams
    it can lead to more than 25 minutes running in order to check all
    modules. For this reason, use multiple threads to run the checks in
    parallel, one per codestream.
    
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>

--- klpbuild/plugins/prepare_tests.py
+++ klpbuild/plugins/prepare_tests.py
@@ -3,20 +3,35 @@
 # Copyright (C) 2025 SUSE
 # Author: Marcos Paulo de Souza <mpdesouza@suse.com>
 
+import concurrent.futures
 import importlib
 import logging
 import os
-from pathlib import Path
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 from natsort import natsorted
 from osctiny import Osc
 
-from klpbuild.klplib.cmd import add_arg_lp_name, add_arg_lp_filter
-from klpbuild.klplib.codestreams_data import get_codestream_by_name, get_codestreams_list
-from klpbuild.klplib.ibs import convert_prj_to_cs, delete_built_rpms, delete_project, do_work, download_binary_rpms, get_projects, prj_prefix, validate_livepatch_module, RPMData
+from klpbuild.klplib.cmd import add_arg_lp_filter, add_arg_lp_name
+from klpbuild.klplib.codestreams_data import (
+    get_codestream_by_name,
+    get_codestreams_list,
+)
+from klpbuild.klplib.config import get_user_settings
+from klpbuild.klplib.ibs import (
+    RPMData,
+    convert_prj_to_cs,
+    delete_built_rpms,
+    delete_project,
+    do_work,
+    download_binary_rpms,
+    get_projects,
+    prj_prefix,
+    validate_livepatch_module,
+)
 from klpbuild.klplib.utils import ARCHS, filter_codestreams, get_tests_path, get_workdir
 
 PLUGIN_CMD = "prepare-tests"
@@ -75,6 +90,32 @@ def download_built_rpms(lp_name, lp_filter):
     logging.info("Download finished.")
 
 
+def validate_module_and_move(cs, arch, lp_name, test_arch_path):
+    """
+    Validate a livepatch module and move it to the test directory.
+
+    Returns the codestream's full product name on success, None on failure.
+    """
+    rpm_dir = Path(cs.get_ccp_dir(lp_name), arch, "rpm")
+    if not rpm_dir.exists():
+        logging.warning("%s/%s: rpm dir not found. Skipping.", cs.full_cs_name(), arch)
+        return None
+
+    # Skip codestreams with more than one rpm, and none. There is an issue elsewhere.
+    rpm_files = list(rpm_dir.rglob("*.rpm"))
+    if len(rpm_files) != 1:
+        logging.warning("%s/%s: expected 1 rpm, found %d. Skipping.", cs.full_cs_name(), arch, len(rpm_files))
+        return None
+
+    # There should be only one rpm, so take the first entry in the list
+    rpm_path = rpm_files[0]
+    rpm_file = rpm_path.name
+    validate_livepatch_module(cs, arch, rpm_dir, rpm_file)
+    shutil.move(rpm_path, Path(test_arch_path, "built"))
+
+    return cs.get_full_product_name()
+
+
 def run(lp_name, lp_filter):
     test_src = get_tests_path(lp_name)
     if test_src and not os.access(test_src, os.X_OK):
@@ -104,24 +145,26 @@ def run(lp_name, lp_filter):
             Path(test_arch_path, d).mkdir(exist_ok=True)
 
         logging.info("Checking %s symbols...", arch)
-        build_cs = []
-        for cs in filter_codestreams(lp_filter, get_codestreams_list()):
-            if arch not in cs.get_default_archs():
-                continue
 
-            rpm_dir = Path(cs.get_ccp_dir(lp_name), arch, "rpm")
-            if not rpm_dir.exists():
-                logging.info("%s/%s: rpm dir not found. Skipping.", cs.full_cs_name(), arch)
-                continue
+        # Prepare list of codestreams to validate
+        cs_to_validate = [
+            cs for cs in filter_codestreams(lp_filter, get_codestreams_list())
+            if arch in cs.get_default_archs()
+        ]
 
-            rpms = list(rpm_dir.glob("**/*.rpm"))
-            assert len(rpms) == 1, f"{cs.full_cs_name()}/{arch}: {len(rpms)} rpms found. Expecting only one"
+        # Validate modules in parallel using processes (not threads) to avoid
+        # thread-safety issues with underlying C libraries
+        workers = int(get_user_settings("workers"))
+        with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
+            futures = [
+                executor.submit(validate_module_and_move, cs, arch, lp_name, test_arch_path)
+                for cs in cs_to_validate
+            ]
+            concurrent.futures.wait(futures)
 
-            rpm_file = rpms[0]
-            validate_livepatch_module(cs, arch, rpm_dir, rpm_file)
-            shutil.copy(Path(rpm_dir, rpm_file), Path(test_arch_path, "built"))
-
-            build_cs.append(cs.get_full_product_name())
+            # Collect successful results
+            results = [future.result() for future in futures]
+            validated_cs = [r for r in results if r is not None]
 
         logging.info("Done.")
 
@@ -141,7 +184,7 @@ def run(lp_name, lp_filter):
             config = Path(test_dst, "config.in")
 
         with open(config, "w") as f:
-            f.write("\n".join(natsorted(build_cs)))
+            f.write("\n".join(natsorted(validated_cs)))
 
         logging.info("Creating %s tar file...", arch)
         subprocess.run(
